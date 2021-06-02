4a48ff8106257f5e3c694d2bddd2a90e0c717f3c
DanielRush <79259903+DanielRush@users.noreply.github.com>
2021-06-02 20:20:16 +0100

    Update getInfo.php

--- php/getInfo.php
+++ php/getInfo.php
@@ -1,5 +1,11 @@
 <?php
 $country_code = $_GET['country_code'];
 $data = file_get_contents("https://restcountries.eu/rest/v2/alpha/$country_code");
-print_r($data);
-                                                           
+
+	$output['status']['code'] = "200";
+    $output['status']['name'] = "ok";
+    $output['status']['description'] = "success";
+    $output['status']['executedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+
+echo($data);
+?>        
