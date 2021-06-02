3c679b0d227a51196f3bf0471c9421862104bb51
DanielRush <79259903+DanielRush@users.noreply.github.com>
2021-06-02 20:18:50 +0100

    Update getCountryInfo.php

--- php/getCountryInfo.php
+++ php/getCountryInfo.php
@@ -10,7 +10,8 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_URL,$url);
 $result=curl_exec($ch);
 curl_close($ch);
-$decode = json_decode($result,true);	
+$decode = json_decode($result,true);
+
 $output['status']['code'] = "200";
 $output['status']['name'] = "ok";
 $output['status']['description'] = "mission saved";
@@ -19,3 +20,5 @@ $output['data'] = $decode['results'];
 
 header('Content-Type: application/json; charset=UTF-8');
 echo json_encode($output);
+
+?>
