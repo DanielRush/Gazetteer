550ab4cca354d424f186dc2cafc8bb4577920a33
DanielRush <79259903+DanielRush@users.noreply.github.com>
2021-06-02 20:20:46 +0100

    Update getUserCountry.php

--- php/getUserCountry.php
+++ php/getUserCountry.php
@@ -2,4 +2,10 @@
 $lat = $_GET['lat'];
 $lng = $_GET['lng'];
 $data = file_get_contents("http://api.geonames.org/countryCodeJSON?lat=$lat&lng=$lng&username=danielrush");
-print_r($data);
+$output['status']['code'] = "200";
+    $output['status']['name'] = "ok";
+    $output['status']['description'] = "success";
+    $output['status']['executedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+
+echo($data);
+?>
